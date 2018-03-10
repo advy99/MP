3354d349d3084a2b566aaf58e31b210a9d26df31
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-03-10 22:50:40 +0100

    comentados ejercicios MiCadenaClasica

--- sesion4/src/MiCadenaClasica.cpp
+++ sesion4/src/MiCadenaClasica.cpp
@@ -3,6 +3,8 @@
 int longitud_cadena( char * cadena){
 	int tamanio = 0;
 
+	// Aumentamos el valor de tamanio hasta encontrar el
+	//caracter de finalizacion '\0'
 	while ( *cadena != '\0' ){
 		tamanio++;
 		cadena++;
@@ -15,8 +17,13 @@ int longitud_cadena( char * cadena){
 bool es_palindromo( char * cadena){
 	bool palindromo = true;
 	int tam_util = longitud_cadena(cadena);
+
+	//Establecemos un puntero al final de la cadena
 	char * p_fin_char = cadena + tam_util - 1;
 
+	// Recorremos la cadena, comparando el primer y ultimo
+	//elemento, hasta que se cruzen los punteros, o encuentre
+	//algun caso que no se cumpla
 	while ( cadena < p_fin_char && palindromo){
 		if(*cadena == *p_fin_char){
 			cadena++;
@@ -31,21 +38,26 @@ bool es_palindromo( char * cadena){
 }
 
 int comparar_cadenas (char * cadena1, char * cadena2){
-	int valor_cad1 = 0;
+	int valor_cad = 0;
 
+	// Añadimos el valor ASCII de la cadena1 a valor_cad
 	for (int i = 0; i < longitud_cadena(cadena1); i++){
-		valor_cad1 += *(cadena1 + i);
-	}
-	for (int i = 0; i < longitud_cadena(cadena2); i++){
-		valor_cad1 -= *(cadena2 + i);
+		valor_cad += *(cadena1 + i);
 	}
 
-	return valor_cad1;
+	// Restamos el valor ASCII de la cadena2 a valor_cad
+	for (int i = 0; i < longitud_cadena(cadena2); i++){
+		valor_cad -= *(cadena2 + i);
+	}
+
+	return valor_cad;
 }
 
 void copiar_cadena(char * resultado, char * original){
 	int longitud = longitud_cadena(original);
 
+	// Con punteros, igualamos las cadenas, y por ultimo asignamos
+	//el valor '\0' al final
 	for(int i = 0; i < longitud ; i++){
 		*(resultado + i) = *(original + i); 
 	}
@@ -56,6 +68,9 @@ void encadenar_cadena (char * original , char * a_copiar ){
 	int longitud_original = longitud_cadena(original);
 	int longitud_a_copiar = longitud_cadena(a_copiar);
 
+	// Añadimos la cadena al final de la original
+	// Al igual que copiar solo que comenzando en el
+	//final de la original
 	for (int i =0; i < longitud_a_copiar; i++){
 		*(original+longitud_original +i) = * (a_copiar + i);
 	}
@@ -66,9 +81,12 @@ void encadenar_cadena (char * original , char * a_copiar ){
 void extraer_subcadena (char * subcadena,char * cadena , int pos , int lon){
 	int lon_original = longitud_cadena(cadena);
 
+	// Comprobamos si la longitud + posicion supera el tamaño
+	//de la cadena
 	if (lon + pos > lon_original)
 		lon = lon_original - pos;
 
+	// Copiamos la parte deseada en subcadena, y añadimos '\0'
 	for (int i = 0; i < lon && i < lon_original; i++){
 		*(subcadena + i) = *(cadena + pos + i);
 	}
@@ -79,6 +97,8 @@ void extraer_subcadena (char * subcadena,char * cadena , int pos , int lon){
 void invertir_cadena (char * invertida ,char * cadena){
 	int longitud = longitud_cadena(cadena);
 
+	// Asignamos el final de cadena al inicio de invertida
+	//saltandonos el ultimo caracter debido a que es '\0'
 	for (int i = 0; i < longitud; i++){
 		*(invertida+i) = *(cadena+longitud - 1 -i);
 	}
