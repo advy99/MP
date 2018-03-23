aaf5da4da455908ebdeb9216d06578da17ffa1f7
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-03-23 18:39:42 +0100

    version final (espero) ejercicio 3 sesion 6

--- sesion06/src/II-VectorDinamicoCadenas.cpp
+++ sesion06/src/II-VectorDinamicoCadenas.cpp
@@ -10,38 +10,55 @@
 #include <iostream>
 #include <cstring>
 
-/*******************************************************************************/
-
-enum TipoRedimension {DeUnoEnUno,EnBloques,Duplicando};
-int * Redimensiona (int * , TipoRedimension , int & );
-
-/*******************************************************************************/
-
-
 using namespace std;
+
+/*******************************************************************************/
+
+char ** SepararCadena(string, int &);
+
+/*******************************************************************************/
 
 int main(){
 	string cadena;
 	string cadena_completa = "";
 	int num_lineas = 0;
-	int tam_cadena_completa;
-	int j = 0;
+	
 
 	while (getline (cin,cadena)){
 		cadena_completa = cadena_completa + cadena;
 		cadena_completa.push_back('\n');
-		num_lineas++;
 	}
 
-	tam_cadena_completa = cadena_completa.size();
+	char ** v = SepararCadena(cadena_completa, num_lineas);
+	
+	
+	for (int i = 0; i < num_lineas; i++){
+		cout << v[i] << endl;
+	}
+
+}
+
+/*******************************************************************************/
+
+char ** SepararCadena(string cadena, int & num_lineas){
+	int tam_cadena_completa;
+	int j = 0;
+
+	tam_cadena_completa = cadena.size();
+
+	for (int i = 0; i < tam_cadena_completa; i++){
+		if (cadena[i] == '\n')	
+			num_lineas++;
+	}
+
 
 	char ** cadenas = new char * [num_lineas];
 
 	for (int i = 0; i < num_lineas; i++){
 		string fila = "";
 
-		while (cadena_completa[j] != '\n'){
-			fila = fila + cadena_completa[j];
+		while (cadena[j] != '\n'){
+			fila = fila + cadena[j];
 			j++;
 		}
 		j++;
@@ -52,39 +69,6 @@ int main(){
 
 		cadenas[i] = linea;
 	}
-	
-	for (int i = 0; i < num_lineas; i++){
-		cout << cadenas[i] << endl;
-	}
 
-}
-
-/*******************************************************************************/
-
-int * Redimensiona (int * p, TipoRedimension tipo, int & cap){
-	
-	const int TAM_BLOQUE = 10;
-	int tam_redimensionado;
-
-	if (tipo == TipoRedimension::DeUnoEnUno){
-		tam_redimensionado = cap + 1;
-		
-	}else if (tipo == TipoRedimension::EnBloques){
-		tam_redimensionado = cap + TAM_BLOQUE;
-
-	}else if (tipo == TipoRedimension::Duplicando){
-		tam_redimensionado = 2 * cap;
-	}
-
-	int * nuevo_vector = new int [tam_redimensionado];
-
-	for (int i = 0; i < cap; i++){
-		nuevo_vector[i] = p[i];
-	}
-
-	cap = tam_redimensionado;
-
-	delete [] p;
-
-	return nuevo_vector;
+	return cadenas;
 }
