9bf34aa9c0269eefa4a85cd1dfa2004fbae7ff92
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-03-24 16:11:21 +0100

    Fix typo

--- sesion06/src/II-VectorDinamicoCadenas.cpp
+++ sesion06/src/II-VectorDinamicoCadenas.cpp
@@ -1,5 +1,5 @@
 /*************************************************************/
-/**																			**/                                                         
+/**                                                         **/                                                         
 /**  Autor : Antonio David Villegas Yeguas                  **/
 /**  1GII - Universidad de Granada                          **/
 /**  Metodologia de la Programacion 2017/18                 **/
