ef97062192ace89e2b0fd1dc5d23463e12dfab54
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-04-07 13:14:49 +0200

    errata ejercicio 9

--- sesion07/src/II_ProblemaAsignacion.cpp
+++ sesion07/src/II_ProblemaAsignacion.cpp
@@ -3,7 +3,7 @@
 /**  Autor : Antonio David Villegas Yeguas                  **/
 /**  1GII - Universidad de Granada                          **/
 /**  Metodologia de la Programacion 2017/18                 **/
-/**  Sesion 7 - Ejercicio 7 - Asignacion tareas             **/
+/**  Sesion 7 - Ejercicio 9 - Asignacion tareas             **/
 /**                          a tecnicos                     **/
 /**                                                         **/
 /*************************************************************/
