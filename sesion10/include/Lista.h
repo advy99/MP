d53d3875a0b5aff3047ccd8a09c1f2aa00b63e5a
Antonio David Villegas Yeguas <advy99@gmail.com>
2018-04-29 14:38:51 +0200

    olvidada cabecera Lista

--- sesion10/include/Lista.h
+++ sesion10/include/Lista.h
@@ -1,3 +1,11 @@
+/*************************************************************/
+/**                                                         **/
+/**  Autor : Antonio David Villegas Yeguas                  **/
+/**  1GII - Universidad de Granada                          **/
+/**  Metodologia de la Programacion 2017/18                 **/
+/**  Sesion 10 - Ejercicio 4 - Lista - Operadores           **/
+/**                                                         **/
+/*************************************************************/
 
 
 #ifndef LISTA_CONSTRUCTOR_COPIA
